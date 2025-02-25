55b435ebfbbed03095f2908a2dd9ad936b6865f8
Sinkler521 <Sinkler521@gmail.com>
2025-02-25 17:45:55 +0200

    Header created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Header} from "@/components/Header/Header";
+import {Suspense} from "react";
+import {Loader} from "@/components/Loader/Loader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <div className="w-full h-screen flex flex-col items-center bg-gray-950">
+          <Header />
+          <Suspense fallback={<Loader />}>
+                  {children}
+          </Suspense>
+      </div>
       </body>
     </html>
   );
